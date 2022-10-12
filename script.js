6e3f5860516667d89cae2827bd527814f6efdedc
drebraga <dre.braga@gmail.com>
2022-10-12 20:47:23 -0300

    Versão 2.1 SeleçãoPratosMelhorada

--- script.js
+++ script.js
@@ -1,77 +1,15 @@
 function selectPrato(i) {
-    let prato = 'prato' + i;
-    let cond = document.getElementById(prato).className;
-    if (i == 1 && cond != 'menuesc-select') {
-        document.getElementById("prato1").className  = "menuesc-select";
-        document.getElementById("prato2").className  = "menuesc";
-        document.getElementById("prato3").className  = "menuesc";
-        document.getElementById("prato4").className  = "menuesc";
-        document.getElementById("prato5").className  = "menuesc";
-        document.getElementById("prato6").className  = "menuesc";
-        document.getElementById("prato7").className  = "menuesc";
+    document.querySelector('check').style.display = 'block'
+    for (let a=1; a<8; a++) {    
+        let prato = '.prato' + a;
+        let elementoInativo = document.querySelector(prato);
+        elementoInativo.classList.remove('select');
     }
-    else if (i == 2 && cond != 'menuesc-select') {
-        document.getElementById("prato1").className  = "menuesc";
-        document.getElementById("prato2").className  = "menuesc-select";
-        document.getElementById("prato3").className  = "menuesc";
-        document.getElementById("prato4").className  = "menuesc";
-        document.getElementById("prato5").className  = "menuesc";
-        document.getElementById("prato6").className  = "menuesc";
-        document.getElementById("prato7").className  = "menuesc";
-    }
-    else if (i == 3 && cond != 'menuesc-select') {
-        document.getElementById("prato1").className  = "menuesc";
-        document.getElementById("prato2").className  = "menuesc";
-        document.getElementById("prato3").className  = "menuesc-select";
-        document.getElementById("prato4").className  = "menuesc";
-        document.getElementById("prato5").className  = "menuesc";
-        document.getElementById("prato6").className  = "menuesc";
-        document.getElementById("prato7").className  = "menuesc";
-    }
-    else if (i == 4 && cond != 'menuesc-select') {
-        document.getElementById("prato1").className  = "menuesc";
-        document.getElementById("prato2").className  = "menuesc";
-        document.getElementById("prato3").className  = "menuesc";
-        document.getElementById("prato4").className  = "menuesc-select";
-        document.getElementById("prato5").className  = "menuesc";
-        document.getElementById("prato6").className  = "menuesc";
-        document.getElementById("prato7").className  = "menuesc";
-    }
-    else if (i == 5 && cond != 'menuesc-select') {
-        document.getElementById("prato1").className  = "menuesc";
-        document.getElementById("prato2").className  = "menuesc";
-        document.getElementById("prato3").className  = "menuesc";
-        document.getElementById("prato4").className  = "menuesc";
-        document.getElementById("prato5").className  = "menuesc-select";
-        document.getElementById("prato6").className  = "menuesc";
-        document.getElementById("prato7").className  = "menuesc";
-    }
-    else if (i == 6 && cond != 'menuesc-select') {
-        document.getElementById("prato1").className  = "menuesc";
-        document.getElementById("prato2").className  = "menuesc";
-        document.getElementById("prato3").className  = "menuesc";
-        document.getElementById("prato4").className  = "menuesc";
-        document.getElementById("prato5").className  = "menuesc";
-        document.getElementById("prato6").className  = "menuesc-select";
-        document.getElementById("prato7").className  = "menuesc";
-    }
-    else if (i == 7 && cond != 'menuesc-select') {
-        document.getElementById("prato1").className  = "menuesc";
-        document.getElementById("prato2").className  = "menuesc";
-        document.getElementById("prato3").className  = "menuesc";
-        document.getElementById("prato4").className  = "menuesc";
-        document.getElementById("prato5").className  = "menuesc";
-        document.getElementById("prato6").className  = "menuesc";
-        document.getElementById("prato7").className  = "menuesc-select";
-    }
-    else {
-        document.getElementById("prato1").className  = "menuesc";
-        document.getElementById("prato2").className  = "menuesc";
-        document.getElementById("prato3").className  = "menuesc";
-        document.getElementById("prato4").className  = "menuesc";
-        document.getElementById("prato5").className  = "menuesc";
-        document.getElementById("prato6").className  = "menuesc";
-        document.getElementById("prato7").className  = "menuesc";
-    }
-
+    let prato = '.prato' + i;
+    let elementoAtivo = document.querySelector(prato);
+    elementoAtivo.classList.toggle('select');
 }
+
+
+
+ 
