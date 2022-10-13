4b711c67eac4604ba7a8eb9e60e16e9b2174db1a
drebraga <dre.braga@gmail.com>
2022-10-12 23:45:04 -0300

    Versão 3.0 SeleçãoMenuUpgrade

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
+
 function selectPrato(i) {
     let prato = '.prato' + i;
     let elementoAtivo = document.querySelector(prato);
-    const cond = Array(elementoAtivo).indexOf('select');
-    for (let a = 1; a < 8; a++) {
-        const pratoi = '.prato' + a;
-        const elementoInativo = document.querySelector(pratoi);
-        elementoInativo.classList.remove('select');
+    let classeElAtv = String(document.querySelector(prato).classList);
+    let res = classeElAtv.indexOf('select');
+    if (res == -1) {
+        for (let a = 1; a < 8; a++) {
+            const pratoi = '.prato' + a;
+            const elementoInativo = document.querySelector(pratoi);
+            elementoInativo.classList.remove('select');
+        }
+        elementoAtivo.classList.add('select');
     }
-    elementoAtivo.classList.add('select');
+    else {
+        elementoAtivo.classList.remove('select');
+    }
+}
 
+function selectBebida(i) {
+    let bebida = '.bebida' + i;
+    let elementoAtivo = document.querySelector(bebida);
+    let classeElAtv = String(document.querySelector(bebida).classList);
+    let res = classeElAtv.indexOf('select');
+    if (res == -1) {
+        for (let a = 1; a < 8; a++) {
+            const bebidai = '.bebida' + a;
+            const elementoInativo = document.querySelector(bebidai);
+            elementoInativo.classList.remove('select');
+        }
+        elementoAtivo.classList.add('select');
+    }
+    else {
+        elementoAtivo.classList.remove('select');
+    }
+}
+
+
+function selectSobremesa(i) {
+    let sobremesa = '.sobremesa' + i;
+    let elementoAtivo = document.querySelector(sobremesa);
+    let classeElAtv = String(document.querySelector(sobremesa).classList);
+    let res = classeElAtv.indexOf('select');
+    if (res == -1) {
+        for (let a = 1; a < 8; a++) {
+            const sobremesai = '.sobremesa' + a;
+            const elementoInativo = document.querySelector(sobremesai);
+            elementoInativo.classList.remove('select');
+        }
+        elementoAtivo.classList.add('select');
+    }
+    else {
+        elementoAtivo.classList.remove('select');
+    }
 }
