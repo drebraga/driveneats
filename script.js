ab76630fc58a144033447d157c9a46cf00e1d6b3
drebraga <dre.braga@gmail.com>
2022-10-12 22:58:13 -0300

    Versão 2.2 SeleçãoPratosMelhorada

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 function selectPrato(i) {
-    document.querySelector('check').style.display = 'block'
-    for (let a=1; a<8; a++) {    
-        let prato = '.prato' + a;
-        let elementoInativo = document.querySelector(prato);
-        elementoInativo.classList.remove('select');
-    }
     let prato = '.prato' + i;
     let elementoAtivo = document.querySelector(prato);
-    elementoAtivo.classList.toggle('select');
+    const cond = Array(elementoAtivo).indexOf('select');
+    for (let a = 1; a < 8; a++) {
+        const pratoi = '.prato' + a;
+        const elementoInativo = document.querySelector(pratoi);
+        elementoInativo.classList.remove('select');
+    }
+    elementoAtivo.classList.add('select');
+
 }
-
-
-
- 
