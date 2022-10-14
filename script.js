73b8ae1dde9ff76e3065a54aef3d3dfbf18587fa
drebraga <dre.braga@gmail.com>
2022-10-14 03:16:43 -0300

    Versão 4.2 BotãoFecharFuncional

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 
 function selectPrato(i) {
-    const prato = '.prato' + i;
-    const elementoAtivo = document.querySelector(prato);
-    const classeElAtv = String(document.querySelector(prato).classList);
-    const res = classeElAtv.indexOf('selectprato');
-    if (res == -1) {
+    const elementoAtivo = document.querySelector('.prato' + i);
+    const res = elementoAtivo.classList.contains('selectprato');
+    if (res == false) {
         for (let a = 1; a < 8; a++) {
-            const pratoi = '.prato' + a;
-            const elementoInativo = document.querySelector(pratoi);
+            const elementoInativo = document.querySelector('.prato' + a);
             elementoInativo.classList.remove('selectprato');
         }
         elementoAtivo.classList.add('selectprato');
@@ -16,15 +13,13 @@ function selectPrato(i) {
         elementoAtivo.classList.remove('selectprato');
     }
 }
+
 function selectBebida(i) {
-    const bebida = '.bebida' + i;
-    const elementoAtivo = document.querySelector(bebida);
-    const classeElAtv = String(document.querySelector(bebida).classList);
-    const res = classeElAtv.indexOf('selectbebida');
-    if (res == -1) {
+    const elementoAtivo = document.querySelector('.bebida' + i);
+    const res = elementoAtivo.classList.contains('selectbebida');
+    if (res == false) {
         for (let a = 1; a < 8; a++) {
-            const bebidai = '.bebida' + a;
-            const elementoInativo = document.querySelector(bebidai);
+            const elementoInativo = document.querySelector('.bebida' + a);
             elementoInativo.classList.remove('selectbebida');
         }
         elementoAtivo.classList.add('selectbebida');
@@ -33,15 +28,13 @@ function selectBebida(i) {
         elementoAtivo.classList.remove('selectbebida');
     }
 }
+
 function selectSobremesa(i) {
-    const sobremesa = '.sobremesa' + i;
-    const elementoAtivo = document.querySelector(sobremesa);
-    const classeElAtv = String(document.querySelector(sobremesa).classList);
-    const res = classeElAtv.indexOf('selectsobremesa');
-    if (res == -1) {
+    const elementoAtivo = document.querySelector('.sobremesa' + i);
+    const res = elementoAtivo.classList.contains('selectsobremesa');
+    if (res == false) {
         for (let a = 1; a < 8; a++) {
-            const sobremesai = '.sobremesa' + a;
-            const elementoInativo = document.querySelector(sobremesai);
+            const elementoInativo = document.querySelector('.sobremesa' + a);
             elementoInativo.classList.remove('selectsobremesa');
         }
         elementoAtivo.classList.add('selectsobremesa');
@@ -50,20 +43,22 @@ function selectSobremesa(i) {
         elementoAtivo.classList.remove('selectsobremesa');
     }
 }
+
 function buttonEnable() {
     // Desabilita o botão
     document.querySelector('.botaoDisable').disabled = true;
     const botaoDisable = document.querySelector('.botaoDisable');
-    document.querySelector('.botaoDisable').value = 'Selecione os 3 itens para fechar o pedido';
+    document.querySelector('.botaoDisable').value = ` Selecione os 3 itens 
+para fechar o pedido`;
     botaoDisable.classList.remove('botaoEnable');
 
-    // Preço do prato
+    // Confere se algum prato foi selecionado
     const precoP = document.querySelector('.selectprato')
 
-    // Preço da Bebida
+    // Confere se alguma bebida foi selecionada
     const precoB = document.querySelector('.selectbebida');
 
-    // Preço da Sobremesa
+    // Confere se alguma sobremesa foi selecionada
     const precoS = document.querySelector('.selectsobremesa');
 
     // Condicional para Habilitar o botão de fechar pedido.
@@ -72,18 +67,36 @@ function buttonEnable() {
         document.querySelector('.botaoDisable').value = 'Fechar pedido';
         const botaoEnable = document.querySelector('.botaoDisable');
         botaoEnable.classList.add('botaoEnable');
-
-        const precoP_string = String(precoP.children[3].innerText).replace(/[^0-9]/g, '');
-        const precoPrato = Number(precoP_string) / 100;
-        console.log(precoPrato);
-        const precoB_string = String(precoB.children[3].innerText).replace(/[^0-9]/g, '');
-        const precoBebida = Number(precoB_string) / 100;
-        console.log(precoBebida);
-        const precoS_string = String(precoS.children[3].innerText).replace(/[^0-9]/g, '');
-        const precoSobre = Number(precoS_string) / 100;
-        console.log(precoSobre);
     }
 }
 function fecharPedido() {
-    console.log('FUNÇÃO PARA ENCAMINHAR CLIENTE PARA O WPP');
+    const precoP = document.querySelector('.selectprato')
+    const precoB = document.querySelector('.selectbebida');
+    const precoS = document.querySelector('.selectsobremesa');
+    // Pega os nomes
+    const nomePrato = String(precoP.children[1].innerText);
+    console.log(nomePrato);
+    const nomeBebida = String(precoB.children[1].innerText);
+    console.log(nomeBebida);
+    const nomeSobre = String(precoS.children[1].innerText);
+    console.log(nomeSobre);
+    // Pega os preços
+    const precoP_string = String(precoP.children[3].innerText).replace(/[^0-9]/g, '');
+    const precoPrato = Number(precoP_string) / 100;
+    const precoB_string = String(precoB.children[3].innerText).replace(/[^0-9]/g, '');
+    const precoBebida = Number(precoB_string) / 100;
+    const precoS_string = String(precoS.children[3].innerText).replace(/[^0-9]/g, '');
+    const precoSobre = Number(precoS_string) / 100;
+    let total = precoPrato + precoBebida + precoSobre;
+    total = total.toFixed(2);
+
+    const uri = `Olá, gostaria de fazer o pedido:
+- Prato: ${nomePrato}
+- Bebida: ${nomeBebida}
+- Sobremesa: ${nomeSobre}
+Total: R$ ${total}`;
+
+    const encoded1 = encodeURIComponent(uri);
+    window.open("https://wa.me/5561981954985?text=" + encoded1, '_blank');
+
 }
